9b444063dd6333f7cb02cb1af17b501696e2cf11
BSDS-777 <b.smith@briansmith.design>
2026-08-21 03:56:09 -0400

    Include comment header in index.php for SEO
    Added a biblical quote for better SEO and context.

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php
 /**
+ * Better SEO
+ * For persons of Faith or Seeking Faith
+ * This file is intentionally left blank to prevent direct directory access.
+ *
+ * @package   Better_SEO
+ * @author    Brian Smith
+ * @copyright 2026 Brian Smith
+ * @license   GPL-2.0-or-later
+ * @since     1.0.0
+ * 
+ * "Of all the commits in all the repos in all the world, grace rewrote mine."
+ * 
+ */
+
+/**
  * Luke 12:6-7 (King James Version)
  * Are not five sparrows sold for two farthings, and not one of them is forgotten before God? But even the very hairs of your head are all numbered. Fear not therefore: ye are of more value than many sparrows.
  * 
